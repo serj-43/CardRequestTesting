79c3dba11aa5ab14df1730d7956fecfc75c281a9
serj-43 <serj-43@yandex.ru>
2022-09-19 21:01:22 +0300

    fixed selectors in negative tests

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 
 import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardOrderTest {
@@ -30,7 +31,7 @@ public class CardOrderTest {
         form.$("[data-test-id=phone] input").setValue("+79991234567");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        form.$("[data-test-id='name']").$(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        form.$("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
@@ -40,7 +41,7 @@ public class CardOrderTest {
         form.$("[data-test-id=phone] input").setValue("79991234567");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        form.$("[data-test-id='phone']").$(".input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        form.$("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
@@ -49,7 +50,7 @@ public class CardOrderTest {
         form.$("[data-test-id=phone] input").setValue("+79991234567");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        form.$("[data-test-id='name']").$(".input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        form.$("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -58,7 +59,7 @@ public class CardOrderTest {
         form.$("[data-test-id=name] input").setValue("Андрей Попов-Петров");
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
-        form.$("[data-test-id='phone']").$(".input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        form.$("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -67,6 +68,6 @@ public class CardOrderTest {
         form.$("[data-test-id=name] input").setValue("Андрей Попов-Петров");
         form.$("[data-test-id=phone] input").setValue("+79991234567");
         form.$("button").click();
-        form.$("[data-test-id=agreement]").$(".input__invalid").isDisplayed();
+        form.$("[data-test-id=agreement].input_invalid").shouldBe(visible);
     }
 }
